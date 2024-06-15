dccf4df5fd833039efcd5fd4e8e6db5c6aac4d68
elephantinforest <sikurowaiyado@gmail.com>
2024-06-15 11:46:45 +0900

    first commit

--- src/src/migration/initialize_all_table.php
+++ src/src/migration/initialize_all_table.php
@@ -1,7 +1,14 @@
 <?php
 require_once '/var/www/html/src/migration/tablequery.php';
+// データベース接続情報を環境変数から取得
+$db = [
+    'hostname' => getenv('MYSQL_HOST'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
+    'database' => getenv('MYSQL_DATABASE'),
+];
 // データベースへの接続
-$mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+$mysqli = new mysqli($db['hostname'], $db['username'], $db['password'], $db['database']);
 if ($mysqli->connect_error) {
   throw new RuntimeException('mysqli接続エラー: ' . $mysqli->connect_error);
 }
